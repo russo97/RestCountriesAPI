97c7ebbcd698c977bf216c890bca4633f1e26d81
Wanderson Silva <wandersonsilva768@gmail.com>
2021-03-07 20:38:05 -0300

    feat: adicionado componente responsável por exibir os países de forma resumida na rota principal

---
+++ src/components/Country.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="country">
+    I am a single country
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Country'
+}
+</script>
+
+<style>
+  @import "../../src/app/scss/styles.scss";
+</style>
