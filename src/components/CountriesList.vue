325ee59317faf02a4f7dd32b3bfa87a53d88f82f
Wanderson Silva <wandersonsilva768@gmail.com>
2021-03-07 20:24:01 -0300

    feat: adicionado componente responsável pela listagem de países

---
+++ src/components/CountriesList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="countrieslist">
+    <filter-area />
+
+    <div class="container">
+      ok
+    </div>
+  </div>
+</template>
+
+<script>
+import FilterArea from './FilterArea.vue';
+
+  export default {
+    components: {
+      FilterArea
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "../../src/app/scss/styles.scss";
+</style>
