8966e9c4decb60350eead5c2fd6e4e02c807c1e1
Wanderson Silva <wandersonsilva768@gmail.com>
2021-03-07 22:25:13 -0300

    feat: listagem inicial dos países disponível no estado global

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -2,22 +2,60 @@
   <div class="countrieslist">
     <filter-area />
 
-    <div class="container">
-      ok
+    <div class="countrieslist__container container">
+      <country
+        :key="country.name"
+        v-for="country in countryList" />
     </div>
   </div>
 </template>
 
 <script>
-import FilterArea from './FilterArea.vue';
+  import { mapActions, mapState } from 'vuex';
+
+  import Country from './Country.vue';
+  import FilterArea from './FilterArea.vue';
 
   export default {
     components: {
+      Country,
       FilterArea
+    },
+
+    mounted () {
+      this.setCountryList();
+    },
+
+    computed: {
+      ...mapState('Countries', [
+        'countryList'
+      ])
+    },
+
+    methods: {
+      ...mapActions('Countries', [
+        'setCountryList'
+      ])
+    },
+
+    watch: {
+      countryList (cur) {
+        console.log('update', cur);
+      }
     }
   }
 </script>
 
 <style lang="scss">
   @import "../../src/app/scss/styles.scss";
+
+  .countrieslist {
+    &__container {
+      @include flex {
+        align-items: center;
+        flex-direction: column;
+        justify-content: space-between;
+      }
+    }
+  }
 </style>
