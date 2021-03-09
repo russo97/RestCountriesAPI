d704c4850ffe394e255f0cf2d7b6858a85ec5053
Wanderson Silva <wandersonsilva768@gmail.com>
2021-03-09 09:57:55 -0300

    feat: adicionada rota para a página de detalhes por país

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,13 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import CountriesList from '../components/CountriesList';
+import CountryDetails from '../components/CountryDetails';
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', component: CountriesList }
+  { path: '/', component: CountriesList },
+  { path: '/country/:numericCode', component: CountryDetails }
 ];
 
 export default new VueRouter({
