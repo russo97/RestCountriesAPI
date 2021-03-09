ec8df72da52a30522f2b753c20e1f923398f62b0
Wanderson Silva <wandersonsilva768@gmail.com>
2021-03-09 09:59:55 -0300

    feat: adicionado componente responsável por exibir as informações detalhadas de um país

---
+++ src/components/CountryDetails.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="countrydetails">
+    {{ $route.params.numericCode }}
+  </div>
+</template>
+
+<script>
+  import { mapState } from 'vuex';
+
+  export default {
+    name: 'CountryDetails',
+
+    computed: {
+      ...mapState('Countries', [
+        'countryList'
+      ])
+    }
+  }
+</script>
+
+<style>
+
+</style>
