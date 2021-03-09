4e48ae12f1ef58fe31ff6251750a1a7cd37f976c
Wanderson Silva <wandersonsilva768@gmail.com>
2021-03-09 10:40:19 -0300

    feat: configurado getter para obter um único país por vez

--- src/store/countries/getters.js
+++ src/store/countries/getters.js
@@ -3,7 +3,12 @@ const isDarkMode = state => state.darkMode;
 
 const regionSelected = state => state.currentRegion.length > 0;
 
+const getCountryByNumericCode = state => numCode => {
+  return state.countryList.filter(country => country.numericCode == numCode)[0];
+};
+
 export default {
   isDarkMode,
-  regionSelected
+  regionSelected,
+  getCountryByNumericCode
 };
