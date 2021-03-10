34a24c8ac77ac63f6d6a98770b8ffa11b84acb23
Wanderson Silva <wandersonsilva768@gmail.com>
2021-03-10 14:26:06 -0300

    feat: estilo dark do botão 'GO BACK HOME'

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,13 +1,67 @@
 <template>
-  <h1>Ops... nada por aqui</h1>
+  <div class="notfound container">
+    <img class="notfound__logo" src="../assets/images/404.svg" alt="404" />
+
+    <button class="notfound__button" :class="{ dark: isDarkMode }" @click="redirectHome">
+      <i class="fas fa-long-arrow-alt-left"></i>
+      Go back home
+    </button>
+  </div>
 </template>
 
 <script>
-export default {
+  import { mapGetters } from 'vuex';
+  import sharedMethods from '../utils/sharedMethods';
 
-}
+  export default {
+    name: 'NotFound',
+
+    computed: {
+      ...mapGetters('Countries', [
+        'isDarkMode'
+      ])
+    },
+
+    extends: sharedMethods
+  }
 </script>
 
-<style>
+<style lang="scss">
+  @import "../../src/app/scss/styles.scss";
 
+  .notfound {
+    position: relative;
+
+    @include flex {
+      @extend %flexFD_C, %flexJC_SB-AI_C;
+    }
+
+    &__logo {
+      width: 90%;
+      margin: 0 auto;
+      margin-top: 3rem;
+
+      @include breakpoint-up (medium) {
+        width: 80%;
+      }
+
+      @include breakpoint-up (desktop) {
+        width: 60%;
+      }
+    }
+
+    &__button {
+      margin-top: 3.5rem;
+
+      @include flex {
+        @extend %flexJC_SB-AI_C;
+      }
+
+      &.dark {
+        color: $white;
+        box-shadow: 0 1px 3px $darkBlue;
+        background-color: $veryDarkBlue;
+      }
+    }
+  }
 </style>
