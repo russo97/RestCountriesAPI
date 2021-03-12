6f91a15eb58ecba103b69aeb9ff049f473a326c0
Wanderson Silva <wandersonsilva768@gmail.com>
2021-03-12 06:20:40 -0300

    feat: incluida datalist com todos os países para auxílio do usuário durante uma pesquisa

---
+++ src/components/CountriesDatalist.vue
@@ -0,0 +1,23 @@
+<template>
+  <datalist id="countryDatalist">
+    <option
+      :value="country.name"
+      :key="country.numericCode"
+      v-for="country in countryList"
+    ></option>
+  </datalist>
+</template>
+
+<script>
+  import { mapState } from 'vuex';
+
+  export default {
+    name: 'CountriesDatalist',
+
+    computed: {
+      ...mapState('Countries', [
+        'countryList'
+      ])
+    }
+  }
+</script>
