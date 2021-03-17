39b615ba8aa5070313bd99e3e928fe50724b4da5
Wanderson Silva <wandersonsilva768@gmail.com>
2021-03-17 00:55:29 -0300

    feat: componente LoadMore será responsável por exibir mais países na lista principal

---
+++ src/components/LoadMore.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="load container" :class="darkModeClass">
+    <div class="load__container">
+      <button class="load__button">
+        Load more
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapState } from 'vuex';
+
+  export default {
+    name: "LoadMore",
+
+    computed: {
+      ...mapState('Countries', [
+        'darkMode'
+      ]),
+
+      darkModeClass () {
+        const { darkMode } = this;
+
+        return { 'dark': darkMode }
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "../../src/app/scss/styles.scss";
+
+  .load {
+    $this: &;
+
+    &__container {
+      @include flex {
+        align-items: center;
+        justify-content: center;
+      }
+    }
+
+    &__button {
+      color: $darkBlue;
+      background-color: $white;
+      box-shadow: 0 2px 4px #aaa;
+    }
+
+    &.dark {
+      #{$this}__button {
+        color: $white;
+        background-color: $darkBlue;
+        box-shadow: 0 2px 4px $veryDarkBlue;
+      }
+    }
+  }
+</style>
